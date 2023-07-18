df9217e73dfa60f119617d63b8306be4d7a5bb7f
yunjin <jed.donaire08@gmail.com>
2023-07-18 20:02:03 +0800

    Make CartQuantity Interactive

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -79,8 +79,17 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       })
     }
 
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity').
+    innerHTML = cartQuantity;
+
     console.log(cart);
-  })
-})
+  });
+});
 
 
